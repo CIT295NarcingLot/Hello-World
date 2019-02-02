a20a8c08f3a10e106d31a0302b7b5e85f158f919
darth_greenedog@hotmail.com <Cit295-BPCC>
2019-02-02 17:57:40 -0600

    Johnny 02Feb19

--- app/src/main/java/com/example/teamtest/MainActivity.kt
+++ app/src/main/java/com/example/teamtest/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //Just adding random commit
         //Testing second commit
+        //Johnny commit!
     }
 }
